8a03fc6a82ece30a4039a8099fe9bd6e54c6ee37
MohamedBidouk <92817844+MohamedBidouk@users.noreply.github.com>
2022-11-22 19:03:26 +0100

    All works properly

--- src/app/services/token.interceptor.ts
+++ src/app/services/token.interceptor.ts
@@ -14,11 +14,19 @@ export class TokenInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const toExclude = "/login"
 
-    let jwt = this.authService.getToken();
+    if(request.url.search(toExclude) === -1){
+      console.log("request = " +request);
+      
+      let jwt = this.authService.getToken();
     let reqWithToken = request.clone( {
     setHeaders: { Authorization : "Bearer "+jwt}
-    } )
+    } );
+    console.log("final request = " +reqWithToken);
       return next.handle(reqWithToken);
+    }
+    return next.handle(request);
+    
   }
 }
